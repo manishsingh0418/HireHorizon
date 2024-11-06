1e803fd21930074ac2508e9e41e183c34f283508
manishsingh0418 <manishkumar80819@gmail.com>
2024-11-07 00:54:49 +0530

    JobCards, Header and Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 import './index.css';
-
+import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import SearchBar from "./components/SearchBar"
+import JobCard from "./components/JobCard"
 function App() {
   return (
     <>
+    <Navbar/>
+    <Header/>
+    <SearchBar/>
+    <JobCard/>
     </>
   );
 }
