0b5fddcde4f4753f63622bc5c39fe9f894d687de
manishsingh0418 <manishkumar80819@gmail.com>
2024-11-07 17:33:49 +0530

    Ordering the data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import './index.css';
+import React, { useState, useEffect } from "react";
+import "./index.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import JobCard from "./components/JobCard";
 import jobData from "./JobDummyData";
-import { collection, query, getDocs } from "firebase/firestore";
+import { collection, query,orderBy, getDocs } from "firebase/firestore";
 import { db } from "./firebase.config";
 
 function App() {
   const [jobs, setJobs] = useState([]);
 
   const fetchJobs = async () => {
-    const q = query(collection(db, "job")); // Changed "jobs" to "job"
-    const querySnapshot = await getDocs(q);
-    const jobList = [];
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.id, " => ", doc.data());
-      jobList.push({ id: doc.id, ...doc.data() });
+    const tempJobs = [];
+    const jobRef = query(collection(db, "job"));
+    const q = query(jobRef , orderBy("postedOn","desc"));
+    const req = await getDocs(q);
+    req.forEach((job) => {
+      tempJobs.push({
+        ...job.data(),
+        id: job.id,
+        postedOn: job.data().postedOn ? job.data().postedOn.toDate() : null,
+      });
     });
-    setJobs(jobList);
+    setJobs(tempJobs);
   };
 
   useEffect(() => {
